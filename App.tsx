66b8bd5abc90fbf8851b77401946571f4e7ee355
bimolynx <bimolynx@gmail.com>
2026-02-08 23:02:10 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -102,7 +102,7 @@ const App: React.FC = () => {
         
         <div className="relative z-10 max-w-7xl mx-auto">
           <div className="max-w-2xl animate-in slide-in-from-left-8 duration-700">
-            <div className="inline-flex items-center gap-2 px-3 py-1 bg-red-light-500 text-black-700 rounded-full text-sm font-semibold mb-6 border border-black-100">
+            <div className="inline-flex items-center gap-2 px-3 py-1 bg-red-500 text-black-700 rounded-full text-sm font-semibold mb-6 border border-black-100">
               <span className="flex h-2 w-2 rounded-full bg-black-600 animate-pulse"></span>
               Disponible 24h/24 sauf le dimanche
             </div>
